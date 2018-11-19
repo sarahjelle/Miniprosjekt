5e2925168b8413da4bde8bb8505ad3f53a8ad478
sarahjel <sarahjel@stud.ntnu.no>
2018-11-19 12:51:20 +0100

    Fikset sql-script

--- server/tests/create_tables.sql
+++ server/tests/create_tables.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS person;
 
 CREATE TABLE artikkel (
-  artikkel_id int(11) NOT NULL AUTO_INCREMENT,
-  tid timestamp CURRENT_TIMESTAMP,
-  overskrift varchar(100) NOT NULL,
-  ingress text NOT NULL,
-  innhold text NOT NULL,
-  kategori int(11) NOT NULL,
-  bilde text NOT NULL,
-  viktighet int(11) NOT NULL,
-  forfatter varchar(50) NOT NULL,
+  artikkel_id INT(11) NOT NULL AUTO_INCREMENT,
+  tid TIMESTAMP CURRENT_TIMESTAMP,
+  overskrift VARCHAR(100) NOT NULL,
+  ingress TEXT NOT NULL,
+  innhold TEXT NOT NULL,
+  kategori INT(11) NOT NULL,
+  bilde TEXT NOT NULL,
+  viktighet INT(11) NOT NULL,
+  forfatter VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 )
 
