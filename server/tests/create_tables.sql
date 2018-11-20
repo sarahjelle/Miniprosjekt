aecdc945f3858552ad43308e1db5f465b24938e7
sarahjel <sarahjel@stud.ntnu.no>
2018-11-20 16:34:22 +0100

    sql syntax -3

--- server/tests/create_tables.sql
+++ server/tests/create_tables.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS artikkel;
 DROP TABLE IF EXISTS kategori;
 
 CREATE TABLE kategori(
-  navn varchar(50) NOT NULL,
-  PRIMARY KEY (navn)
+  navn varchar(50) NOT NULL
 );
+ALTER TABLE kategori
+  ADD PRIMARY KEY (navn);
 
 CREATE TABLE artikkel (
   artikkel_id INT(11) NOT NULL AUTO_INCREMENT,
@@ -16,16 +17,21 @@ CREATE TABLE artikkel (
   kategori VARCHAR(50) NOT NULL,
   bilde TEXT NOT NULL,
   viktighet INT(11) NOT NULL,
-  forfatter VARCHAR(50) NOT NULL,
-  PRIMARY KEY (artikkel_id),
-  KEY cat_fk (kategori),
-      CONSTRAINT cat_fk FOREIGN KEY (kategori) REFERENCES kategori (navn)
-);
+  forfatter VARCHAR(50) NOT NULL
+ );
+
+ ALTER TABLE artikkel
+   ADD PRIMARY KEY (artikkel_id);
+
+
 
 CREATE TABLE likes(
-    artikkel_id INT(11) NOT NULL,
-    antall INT DEFAULT 0,
-    PRIMARY KEY(artikkel_id),
-    KEY id_fk (artikkel_id),
-    CONSTRAINT id_fk FOREIGN KEY (artikkel_id) REFERENCES artikkel (artikkel_id)
+  artikkel_id int(11) NOT NULL,
+  antall int(11) DEFAULT '0'
 );
+
+ALTER TABLE likes
+  ADD PRIMARY KEY (artikkel_id);
+
+ALTER TABLE likes
+  ADD CONSTRAINT id_fk FOREIGN KEY (artikkel_id) REFERENCES artikkel (artikkel_id);
